4c2a113547b680716796bccddcc246e16ca8329c
Allan Holm Søndergaard <6605392@rts-365.dk>
2020-09-21 15:14:30 +0200

    added functionality for imgSwitch

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,7 +1,8 @@
 import footer from "./footer.js";
-import {changeImg} from "./imgSwitch.js";
+import { changeImg, fillContentSwitch } from "./imgSwitch.js";
 
 const switchButtons = document.querySelectorAll(".imgSwitch svg");
 switchButtons.forEach(button => button.addEventListener("click", changeImg));
 
+fillContentSwitch();
 footer();
