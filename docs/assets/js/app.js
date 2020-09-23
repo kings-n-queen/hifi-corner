792a04f10786ede084564c20bf9f6fcc8eabc384
Lui Fensmark Nielsencdcd..git config --global user.email 3862142@rts-365.dk <3862142@rts-365.dk>
2020-09-23 14:29:52 +0200

    staged versioner

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,8 +1,11 @@
 import footer from "./footer.js";
 import { changeImg, fillContentSwitch } from "./imgSwitch.js";
+import search from "./searchbarfunction.js";
 
 const switchButtons = document.querySelectorAll(".imgSwitch svg");
 switchButtons.forEach(button => button.addEventListener("click", changeImg));
 
 fillContentSwitch();
 footer();
+search();
+
