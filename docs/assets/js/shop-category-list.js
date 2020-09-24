cc18602e3b0ccadc81e43da5596dca0872bb2c19
Isabella Robinson <134569@RTS-365.dk>
2020-09-24 13:27:00 +0200

    shop-catefory-list handler

--- docs/assets/js/shop-category-list.js
+++ docs/assets/js/shop-category-list.js
@@ -9,6 +9,7 @@ import footer from "./footer.js";
 import filterProductsByPrice from "./shop-category-list-filterByPrice.js";
 import filterProductsByManufacturer from "./shop-category-list-filterByManufacturer.js";
 import {numbersForFilterPrice, numbersForFilterManufacturer, displayNumbers} from "./shop-category-list-filterNumbers.js"
+import categoryHandler from "./shop-category-list-amplifiers.js"
 
 fetchProducts().then(function(jsonObj) {
     console.log(urlGetKey("product"));
