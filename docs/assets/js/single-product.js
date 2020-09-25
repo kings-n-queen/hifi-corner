c932e73258c93aa6974a78b5da38c1c867aa77fd
Allan Sondergaard <allan_2233@hotmail.com>
2020-09-25 10:10:01 +0200

    json and single product func

--- docs/assets/js/single-product.js
+++ docs/assets/js/single-product.js
@@ -21,7 +21,7 @@ fetchProducts().then(function(products) {
     console.log(urlGetKey("id"));
     console.log(product);
 
-    productDetailsName(product.navn, product.beskrivelse, product.category, product.andreProdukter, product.pris);
+    productDetailsName(product.navn, product.beskrivelse, product.manufacturer, product.andreProdukter, product.pris);
     specs(product.description);
 });
 
