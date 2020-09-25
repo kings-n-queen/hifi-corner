18869e5229312167ce9b284037d9a0c6f383bcd8
ronny christensen <6382104@rts-365.dk>
2020-09-25 09:29:04 +0200

    Update single-product.js
    image fix + breadcrumbs

--- docs/assets/js/single-product.js
+++ docs/assets/js/single-product.js
@@ -9,71 +9,64 @@ import productDetailsName from "./single-product-details.js";
 import search from "./searchbarfunction.js";
 
 
-fetchProducts().then(function(products) {
+fetchProducts().then(function(jsonObj) {
     var id = urlGetKey("id");
     var product;
-    for (let i = 0; i < products.products.length; i++) {
-        if (products.products[i].id == id) {
-            product = products.products[i];
+    for (let i = 0; i < jsonObj.products.length; i++) {
+        if (jsonObj.products[i].id == id) {
+            product = jsonObj.products[i];
             break
         }
     }
-    console.log(urlGetKey("id"));
-    console.log(product);
+
 
     productDetailsName(product.navn, product.beskrivelse, product.category, product.andreProdukter, product.pris);
     specs(product.description);
+    singlePageImage(product);
+    additionalInfo(product);
+    var categoryLink = `shop-category-list.html?category=${product.category}`
+    setBreadcrumbs(product.category, categoryLink, product.navn);
+
+
 });
 
-let dummyInfo = {
-    name: "test",
-    permalink: "single-product-description.html?product=marantz-pm6006",
-    additionalInformations: {
-        manufacturer: "Marantz",
-        manufacturerLink: "Marantz CD Player CD6007/T1",
-        freeWarranty: "3 years",
-        deliveryCharge: "Free",
-        deliveryTime: "1-5 Working days",
-        cardSurcharges: "No"
-    }
+
+function setBreadcrumbs(title, permalink, productName) {
+    let breadCrumbArray = [{ title: "Home", permalink: "index.html" }];
+    breadCrumbArray.push({ title, permalink });
+    breadCrumbArray.push({ title: productName, permalink: "" });
+    breadcrumbs(breadCrumbArray);
 }
 
-additionalInfo(dummyInfo);
+function arrange(jsonObj) {
 
-let dummyImages = {
-    imgSrc: [
-        "./assets/images/lps/Pro_ject_Debut_3_bl.jpg",
-        "./assets/images/lps/Pro_ject_Debut_III_red_1.jpg",
-        "./assets/images/lps/Pro_ject_Debut_III_yellow_1.jpg"
-    ]
+
+
+
+
+    //#region BREADCRUMBS
+
+
+    // let dummyBreadcrumbArray = [];
+
+    // addBreadcrumbItem(dummyBreadcrumbArray, "Home", "index.html");
+    // addBreadcrumbItem(dummyBreadcrumbArray, "Amplifiers", "?category=amplifiers");
+    // addBreadcrumbItem(dummyBreadcrumbArray, "Power Amplifiers", "?category=power-amplifiers");
+    // addBreadcrumbItem(dummyBreadcrumbArray, "MANLEY MAHI POWER AMPLIFIER", "");
+
+    // breadcrumbs(dummyBreadcrumbArray);
+
+    // function addBreadcrumbItem(toArray, title, permalink) {
+    //     toArray.push({ title, permalink });
+    // }
+
+    //#endregion BREADCRUMBS
+
+    const thumbnails = document.querySelectorAll(".thumbnailContainer__image");
+    thumbnails.forEach(image => image.addEventListener("click", changeLargeImg));
+
+
+    footer();
+    search();
+
 }
-
-
-singlePageImage(dummyImages);
-
-
-additionalInfo(dummyInfo);
-
-//#region BREADCRUMBS
-
-let dummyBreadcrumbArray = [];
-
-addBreadcrumbItem(dummyBreadcrumbArray, "Home", "index.html");
-addBreadcrumbItem(dummyBreadcrumbArray, "Amplifiers", "?category=amplifiers");
-addBreadcrumbItem(dummyBreadcrumbArray, "Power Amplifiers", "?category=power-amplifiers");
-addBreadcrumbItem(dummyBreadcrumbArray, "MANLEY MAHI POWER AMPLIFIER", "");
-
-breadcrumbs(dummyBreadcrumbArray);
-
-function addBreadcrumbItem(toArray, title, permalink) {
-    toArray.push({ title, permalink });
-}
-
-//#endregion BREADCRUMBS
-
-const thumbnails = document.querySelectorAll(".thumbnailContainer__image");
-thumbnails.forEach(image => image.addEventListener("click", changeLargeImg));
-
-
-footer();
-search();
